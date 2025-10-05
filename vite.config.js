e512736d66b1ec6867f6959ea168cd23106e1266
Tushar Sarangi <tush23@live.com>
2025-10-05 14:40:11 +0530

    Deploy built files to main

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "./", // <-- use relative paths for main branch deployment
   plugins: [react()],
 })
